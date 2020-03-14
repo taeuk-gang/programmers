1d5e72f3ab9b4c6a39813f29d40510a8f2f52b05
taeuk-gang <taeuk_kang@naver.com>
2020-03-14 14:21:00 +0900

    :up: 모달 창 사라지는 기능 업데이트
    - 모달 영역 밖 / ESC / X 누를 때 모달창 사라짐

--- src/ImageInfo.js
+++ src/ImageInfo.js
@@ -16,6 +16,11 @@ class ImageInfo {
   setState(nextData) {
     this.data = nextData;
     this.render();
+
+    document.querySelector(`.ImageInfo`).addEventListener(`click`, this.clickModalWrap.bind(this));
+    document.querySelector(`.ImageInfo .close`).addEventListener(`click`, this.clickX.bind(this));
+
+    document.addEventListener(`keydown`, this.keydownESC.bind(this));
   }
 
   render() {
@@ -38,5 +43,39 @@ class ImageInfo {
     } else {
       this.$imageInfo.style.display = "none";
     }
+  }
+  
+  // # 모달 클릭시
+  clickModalWrap(event) {
+    const target = event.target;
+		if (!target.classList.contains(`ImageInfo`)) {
+			return;
+    }
+    
+    this.hide();
+  }
+
+  // # X 클릭시
+  clickX(event) {
+    const target = event.target;
+		if (!target.classList.contains(`close`)) {
+			return;
+    }
+    
+    this.hide();
+  }
+  
+  // # ESC 입력시
+  keydownESC(event) {
+    const isEsc = () => event.key === `Escape`;
+
+    if (isEsc()) {
+      this.hide();
+    }
+  }
+
+  // # custom function
+  hide() {
+    this.$imageInfo.style.display = `none`;
   }
 }
