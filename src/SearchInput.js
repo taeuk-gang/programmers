a45c9c2b95565a130a431f5c2ce2c1a3e12089bc
taeuk-gang <taeuk_kang@naver.com>
2020-03-14 14:53:42 +0900

    :up: 서치창 - 오토포커스 및 클릭시 입력값 초기화

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -3,8 +3,10 @@ const TEMPLATE = '<input type="text">';
 class SearchInput {
   constructor({ $target, onSearch }) {
     const $searchInput = document.createElement("input");
-    this.$searchInput = $searchInput;
+    this.$searchInput = $searchInput;    
     this.$searchInput.placeholder = "고양이를 검색해보세요.|";
+    this.$searchInput.type = "search";
+    this.$searchInput.autofocus = "true";
 
     $searchInput.className = "SearchInput";
     $target.appendChild($searchInput);
@@ -13,6 +15,12 @@ class SearchInput {
       if (e.keyCode === 13) {
         onSearch(e.target.value);
       }
+    });
+
+    $searchInput.addEventListener("click", e => {
+      const empty = () => e.target.value = ``;
+
+      empty();
     });
 
     console.log("SearchInput created.", this);
